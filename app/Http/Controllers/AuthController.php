cec6376f2d579a46825fb5b338d5f8c9f8d28f72
Gabriel-otirB <lordglayder@gmail.com>
2025-01-23 15:33:13 -0300

    Login errors and keep login input values

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,24 @@ class AuthController extends Controller
     {
         return view('login');
     }
+
+    public function loginSubmit(Request $request)
+    {
+        // FORM VALIDATION
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required',
+            ]
+        );
+
+        // GET USER INPUT
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK, validado!';
+    }
+
     public function logout()
     {
         echo 'Logout';
